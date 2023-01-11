fbcaee1785c3d501985b36d60006954f2764670c
GZaranza <Gabriel Zaranza@DESKTOP-2BT978U>
2023-01-10 21:04:14 -0300

    primeiros passos das telas

---
+++ Trabalho_projeto/src/view/TelaMenu.java
@@ -0,0 +1,69 @@
+package view;
+
+import java.awt.Font;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class TelaMenu implements ActionListener{
+	
+	private static JFrame janela = new JFrame("Sistema de venda de carros");
+	private static JLabel titulo = new JLabel("Menu Principal");
+	private static JButton loja = new JButton("Lojas");
+	private static JButton usuario = new JButton("Usuarios");
+	
+	public TelaMenu() {
+		titulo.setFont(new Font("Arial", Font.BOLD, 20));
+		titulo.setBounds(120, 10, 150, 30);
+		loja.setBounds(140, 100, 100, 30);
+		usuario.setBounds(140, 150, 100, 30);
+		
+		
+		janela.setLayout(null);
+		
+		janela.add(titulo);
+		janela.add(loja);
+		janela.add(usuario);
+		;
+		
+		janela.setSize(400, 250);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TelaMenu menu = new TelaMenu();
+		loja.addActionListener(menu);
+		usuario.addActionListener(menu);
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Object src = e.getSource();
+		
+		if(src == loja) {
+			JFrame testeLoja = new JFrame("Teste Loja");
+			JLabel msgLoja = new JLabel("VOCÊ SELECIONOU A OPÇÃO DE LOJA");
+			msgLoja.setBounds(120, 10, 150, 30);
+			testeLoja.add(msgLoja);
+			
+			testeLoja.setSize(400, 250);
+			testeLoja.setVisible(true);
+		}
+		
+		if(src == usuario) {
+			JFrame testeUsu = new JFrame("Teste Loja");
+			JLabel msgUsu = new JLabel("VOCÊ SELECIONOU A OPÇÃO DE USUARIO");
+			msgUsu.setBounds(120, 10, 150, 30);
+			testeUsu.add(msgUsu);
+			
+			testeUsu.setSize(400, 250);
+			testeUsu.setVisible(true);
+		}
+	}
+
+}
